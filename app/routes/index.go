ebce1bb2668c9132fe817ad0c3855cc7dbfee102
darcdev <diegoandresrojas2000@gmail.com>
2023-06-13 17:10:40 -0500

    refactor : move routes to own directory

---
+++ app/routes/index.go
@@ -0,0 +1,17 @@
+package routes
+
+import (
+	"net/http"
+	"virtual-plant-nursery-go/app/controllers"
+)
+
+func Routes() {
+	// Rutas de la API
+	http.HandleFunc("/plants", controllers.GetAllPlants)
+	http.HandleFunc("/plant", controllers.GetPlant)
+	http.HandleFunc("/plant/create", controllers.CreatePlant)
+	http.HandleFunc("/plant/update", controllers.UpdatePlant)
+	http.HandleFunc("/plant/delete", controllers.DeletePlant)
+	http.HandleFunc("/plants/water", controllers.AddWaterToPlants)
+	http.HandleFunc("/plants/nutrients", controllers.AddNutrientsToPlants)
+}
--- app/start.go
+++ app/start.go
@@ -3,20 +3,12 @@ package app
 import (
 	"log"
 	"net/http"
-	"virtual-plant-nursery-go/app/controllers"
+	"virtual-plant-nursery-go/app/routes"
 )
 
 func Start() {
-	// Rutas de la API
-	http.HandleFunc("/plants", controllers.GetAllPlants)
-	http.HandleFunc("/plant", controllers.GetPlant)
-	http.HandleFunc("/plant/create", controllers.CreatePlant)
-	http.HandleFunc("/plant/update", controllers.UpdatePlant)
-	http.HandleFunc("/plant/delete", controllers.DeletePlant)
-	http.HandleFunc("/plants/water", controllers.AddWaterToPlants)
-	http.HandleFunc("/plants/nutrients", controllers.AddNutrientsToPlants)
-
+	routes.Routes()
 	// Iniciar el servidor en el puerto 8080
+	log.Println("Iniciando Servidor en el puerto 8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
-
 }
