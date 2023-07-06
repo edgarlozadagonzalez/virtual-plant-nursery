84200c222a888b762c5cef70d26a985959586c79
Johan98-dev <johanalbarracin67@gmail.com>
2023-07-05 19:42:05 -0500

    Se creó la conexión a la BD

---
+++ Backend/api-rest/database/connection.go
@@ -0,0 +1,21 @@
+package database
+
+import (
+	"fmt"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+)
+
+// InitDB initializes the database connection
+func InitDB() *gorm.DB {
+	connectionString := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=require",
+		dbHost, dbPort, dbUser, dbPassword, dbName)
+
+	db, err := gorm.Open("postgres", connectionString)
+	if err != nil {
+		panic("Failed to connect to the database!")
+	}
+
+	return db
+}
