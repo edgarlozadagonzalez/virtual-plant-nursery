5dccec18b196cb6873d8e8ad8dde5ec0f4a8555a
Edgar David Lozada González <110056560+edgarlozadagonzalez@users.noreply.github.com>
2023-08-29 12:56:57 -0500

    Update config.go

--- Backend/api-rest/database/config.go
+++ Backend/api-rest/database/config.go
@@ -3,7 +3,7 @@ package database
 const (
 	dbHost     = "pg-nursery-virtual-plant-nursery.aivencloud.com"
 	dbPort     = 24037
-	dbUser     = "avnadmin"
-	dbPassword = "AVNS_LNY0EgWwgzGSDpve8Bg"
+	dbUser     = "******"
+	dbPassword = "******"
 	dbName     = "nursery"
 )
